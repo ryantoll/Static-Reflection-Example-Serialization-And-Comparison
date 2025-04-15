104af27f2f8f07e1453c1be19f8dbf7a3e3ff3b3
Ryan Toll <ryan.toll@protonmail.com>
2025-04-15 09:58:45 -0400

    Unit Test Serialization
    - Add unit tests for serialization

--- tests/test.cpp
+++ tests/test.cpp
@@ -14,6 +14,19 @@ TEST_CASE("Constexpr (in)equality") {
     STATIC_REQUIRE(myVar != fieldThreeMismatch);
 }
 
+TEST_CASE("Serialization (in)equality") {
+    constexpr auto myVar = FOO{ 1, "abc", '-' };
+    constexpr auto exactMatch = FOO{ 1, "abc", '-' };
+    constexpr auto fieldOneMismatch = FOO{ 2, "abc", '-' };
+    constexpr auto fieldTwoMismatch = FOO{ 1, "cba", '-' };
+    constexpr auto fieldThreeMismatch = FOO{ 1, "abc", '*' };
+
+    REQUIRE(myVar.serialize() == exactMatch.serialize());
+    REQUIRE_FALSE(myVar.serialize() == fieldOneMismatch.serialize());
+    REQUIRE_FALSE(myVar.serialize() == fieldTwoMismatch.serialize());
+    REQUIRE_FALSE(myVar.serialize() == fieldThreeMismatch.serialize());
+}
+
 // This struct has one field deliberately unmapped
 struct BAR : public SERIALIZATION<BAR>, LEXICOGRAPHICAL_EQUALITY<BAR> {
     int one;
@@ -38,4 +51,17 @@ TEST_CASE("Equality only checks fields declared in mapping") {
     STATIC_REQUIRE(myVar == ignoredFieldMismatch); // Field "ignoreMe" is mismatched, but is not mapped
     STATIC_REQUIRE(myVar != fieldOneMismatch); // Field "one" is mismatched
     STATIC_REQUIRE(myVar != fieldTwoMismatch); // Field "two" is mismatched
+}
+
+TEST_CASE("Serialization only includes fields declared in mapping") {
+    constexpr auto myVar = BAR{ 1, "abc", "Extra notes" };
+    constexpr auto exactMatch = BAR{ 1, "abc", "Extra notes" };
+    constexpr auto ignoredFieldMismatch = BAR{ 1, "abc", "Extra notes do not participate in equality check." };
+    constexpr auto fieldOneMismatch = BAR{ 3, "abc", "N/A" };
+    constexpr auto fieldTwoMismatch = BAR{ 1, "4", "N/A" };
+
+    REQUIRE(myVar.serialize() == exactMatch.serialize()); // Exact match
+    REQUIRE(myVar.serialize() == ignoredFieldMismatch.serialize()); // Field "ignoreMe" is mismatched, but is not mapped
+    REQUIRE_FALSE(myVar.serialize() == fieldOneMismatch.serialize()); // Field "one" is mismatched
+    REQUIRE_FALSE(myVar.serialize() == fieldTwoMismatch.serialize()); // Field "two" is mismatched
 }
